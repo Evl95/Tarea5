bba33e2d239af13aef44383324ff53c2c229c000
Evl95 <100544685+Evl95@users.noreply.github.com>
2022-02-27 19:34:06 -0600

    Tarea 5 de laboratorio progra 092
    Programa que encuentra el factorial de un número y muestra cuantos multiplos de 4 hay en ese factorial.

---
+++ tarea5/tarea5/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'PRIMERO REALIZAMOS EL CALCULO DEL FACTORIAL
+
+        Dim lector As Integer 'variable que guarda el numero en el textbox
+        Dim numero As Integer 'variable para los ciclos del factorial
+        Dim numfacto As Integer 'var para el factorial
+        Dim res1 As String = ""
+        Dim cantmult4 As Integer = 0
+        numfacto = 1
+        lector = TextBox1.Text 'leemos el textbox
+        'realizamos iteraciones tantas veces como el numero al que queremos
+        'averiguar el factorial
+        For numero = 1 To lector Step 1
+            numfacto = numfacto * numero
+        Next
+
+
+        'UNA VEZ TENEMOS EL FACTORIAL CREAMOS UN CICLO QUE CALCULE LOS
+        'MULTIPLOS DE 4
+
+        Dim indice = 0
+        While (indice <= numfacto) 'El ciclo se repite tanto como el factorial
+
+
+            If (indice Mod 4 = 0) Then 'Condicion de factores de 4
+                res1 = res1 + indice.ToString + "," 'Esto muestra los 
+                'factores UNO POR UNO en una textbox
+
+                cantmult4 = cantmult4 + 1 'Esto encuentra el TOTAL de factores
+                'que cumplen la condicion
+            End If
+
+            indice = indice + 1 'Suma 1 al ciclo
+        End While
+
+        'Visualización:
+        TxtRES.Text = res1
+        MsgBox("Cantidad de multiplos de 4= " & cantmult4.ToString)
+
+    End Sub
+End Class
---
+++ tarea5/tarea5/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("tarea5")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard")>
+<Assembly: AssemblyProduct("tarea5")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("7a56ddaf-2527-48c3-9110-bf9a75026b33")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
